fe0127ad6366d47ed8b67e9a78dbc9f7300ce92f
Scott Smith <ssmith@tmvse.com>
2020-12-11 22:28:50 +0000

    day9 done

--- src/day9.cpp
+++ src/day9.cpp
@@ -28,6 +28,8 @@ void main()
 {
     auto numbers = parse_list("../src/day9_input.txt");
 
+    int number_pos;
+    size_t dodgy_number;
     int preamble = 25;
 
     for(int i=preamble; i<numbers.size(); ++i)
@@ -57,7 +59,29 @@ void main()
         } while (std::prev_permutation(indexes.begin(), indexes.end()));
 
         if(!found){
-            std::cout << "found the bam: " << numbers[i] << std::endl;
+            number_pos = i;
+            dodgy_number = numbers[i];
+            std::cout << "found the dodgy number: " << dodgy_number << std::endl;
         }
     }
+
+
+    for(int i=2; i<number_pos; ++i){
+        int num_seqs = number_pos-(i-i);
+        for(int j=0; j<num_seqs; ++j){
+            size_t sum = std::accumulate(numbers.begin()+j,numbers.begin()+j+i,0ULL);
+            if(sum == dodgy_number){
+                size_t mn = std::numeric_limits<size_t>::max();
+                size_t mx = std::numeric_limits<size_t>::min();
+                for(auto it=numbers.begin()+j; it!=numbers.begin()+j+i; ++it){
+                    mn = std::min(mn, *it);
+                    mx = std::max(mx, *it);
+                    std::cout << *it << " ";
+                }
+                std::cout << "= " << sum << std::endl;
+                std::cout << "min + max: " << mn  << " + " << mx << " = " << mn+mx << std::endl;
+            }
+        }
+    }
+
 }
