136044c92a89407b22e3054201f6ec5694216a04
Scott Smith <ssmith@tmvse.com>
2020-12-11 12:52:53 +0000

    day 6 done

---
+++ src/day6.cpp
@@ -0,0 +1,80 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+std::vector<std::map<char,int>> parse_txt(const std::string& file){
+    std::vector<std::map<char,int>> ret;
+
+    std::ifstream test_answers(file);
+    if(!test_answers){
+        std::cout << "cannot open file" << std::endl;
+        return ret;
+    }
+
+    ret.push_back(std::map<char,int>());
+    std::string line;
+    while (std::getline(test_answers, line)) {
+        std::istringstream ss_line(line);
+        if(line == ""){
+            ret.push_back(std::map<char,int>());
+            continue;
+        }
+        while(ss_line){
+            char answer;
+            ss_line >> answer;
+            ret.back()[answer]++;
+        }
+    }
+
+    return ret;
+}
+
+std::vector<std::pair<int,std::map<char,int>>> parse_txt2(const std::string& file){
+    std::vector<std::pair<int,std::map<char,int>>> ret;
+
+    std::ifstream test_answers(file);
+    if(!test_answers){
+        std::cout << "cannot open file" << std::endl;
+        return ret;
+    }
+
+    ret.push_back(std::pair<int,std::map<char,int>>());
+    std::string line;
+    while (std::getline(test_answers, line)) {
+        std::istringstream ss_line(line);
+        if(line == ""){
+            ret.push_back(std::pair<int,std::map<char,int>>());
+            ret.back().first = 0;
+            continue;
+        }
+        char answer;
+        while(ss_line >> answer){
+            ret.back().second[answer]++;
+        }
+        ret.back().first++;
+    }
+
+    return ret;
+}
+
+void main()
+{
+    auto groups = parse_txt2("../src/day6_input.txt");
+
+    int sum = 0;
+    for(auto group : groups){
+        int sub_sum = 0;
+        for(auto p : group.second){
+            if(group.first == p.second){
+                sub_sum++;
+            }
+        }
+        sum += sub_sum;
+    }
+    std::cout << sum << "\n";
+}
